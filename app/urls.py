6eaf737da7daceaea72e8ac392df1cd383303c59
judeakinwale <judeakinwale@gmail.com>
2022-04-13 21:03:21 +0100

    Update config for local static files

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -22,3 +23,6 @@ urlpatterns = [
     path('auth/', include('user.urls', namespace='user')),
     path("password_reset/", include('django_rest_passwordreset.urls', namespace='password_reset')), # For django-rest-passwordreset
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
