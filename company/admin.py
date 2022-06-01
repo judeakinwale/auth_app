9cc0f165ba985e093f65f479757b6c91c9c08b99
judeakinwale <judeakinwale@gmail.com>
2022-06-01 12:18:02 +0100

    Minor Changes

--- company/admin.py
+++ company/admin.py
@@ -3,6 +3,18 @@ from company import models
 
 # Register your models here.
 
+class EventAdmin(admin.ModelAdmin):
+  list_display = [
+    "company",
+    "client",
+    "employee",
+    "end_date",
+    "date",
+    "is_active",
+    "status",
+  ]
+
+
 admin.site.register(models.Company)
 admin.site.register(models.Phone)
 admin.site.register(models.Branch)
@@ -10,7 +22,7 @@ admin.site.register(models.Department)
 admin.site.register(models.Employee)
 admin.site.register(models.Location)
 admin.site.register(models.Client)
-admin.site.register(models.Event)
+admin.site.register(models.Event, EventAdmin)
 admin.site.register(models.Month)
 admin.site.register(models.Schedule)
 admin.site.register(models.Week)
