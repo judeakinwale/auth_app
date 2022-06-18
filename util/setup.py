ee5fa53230c781b2ac40274862c41c4ca0f1cba6
judeakinwale <judeakinwale@gmail.com>
2022-06-18 13:20:50 +0100

    Fix bug with generate employee id

--- util/setup.py
+++ util/setup.py
@@ -18,7 +18,7 @@ def company_first_time_setup(request, company=None):
     "company": company,
     "role": "Admin",
   }
-  defaults = {"employee_id": generate_employee_id("EMP"),}
+  defaults = {"employee_id": generate_employee_id(company," EMP"),}
   employee, created = models.Employee.objects.get_or_create(**payload, defaults=defaults)
   print("Employee instance created for company admin" if created else "company admin updated")
   
@@ -28,13 +28,23 @@ def company_first_time_setup(request, company=None):
   print("First Time Setup Completed")
   return True  
   
+
+def get_larget_employee_id(company, **kwargs) -> int:
+  sep = kwargs.get("seperator", " ")
+  employees = models.Employee.objects.filter(company=company)
+  if not employees:
+    return 0
+  employee_id_list = [int(emp.employee_id.split(sep)[1]) for emp in employees]
+  max_id  = max(employee_id_list)
+  return max_id
   
-def generate_employee_id(prefix:str, length:int=5, seperator:str=" ") -> str:
+def generate_employee_id(company, prefix:str, length:int=5, seperator:str=" ") -> str:
   try:
-    latest_employee = models.Employee.objects.all().sort("-created_at")[0]
-    latest_employee_id = latest_employee.employee_id
-    string_id = latest_employee_id.split(seperator)[1]
-    id = int(string_id)
+    # latest_employee = models.Employee.objects.all().sort("-created_at")[0]
+    # latest_employee_id = latest_employee.employee_id
+    # string_id = latest_employee_id.split(seperator)[1]
+    # id = int(string_id)
+    id = get_larget_employee_id(company)
     id += 1
     str_id = str(id).zfill(length)
     new_id = f"{prefix}{seperator}{id}"
@@ -43,7 +53,8 @@ def generate_employee_id(prefix:str, length:int=5, seperator:str=" ") -> str:
       return new_id
   except Exception as e:
     print(f"Exception while generating employee id: {e}")
-    generate_employee_id(prefix, length)
+    # generate_employee_id(prefix, length)
+    return None
     
     
 def create_months_for_current_year(company: models.Company) -> bool:
