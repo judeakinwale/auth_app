5d6dca9c5ec1e7a53ed9f17c8c58c808356668ae
judeakinwale <judeakinwale@gmail.com>
2022-06-29 23:01:21 +0100

    Update template settings and .gitignore

--- app/settings.py
+++ app/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
